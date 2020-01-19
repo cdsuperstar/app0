42631935b18391153f92eb55ca56f495e52c00c3
Luke Lin <181977814@qq.com>
2020-01-19 14:31:21 +0800

    Add Ide configs.

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: 'es5', // 数组 对象尾随逗号 [arr, ] { obj, }
+  bracketSpacing: true, // 对象空格 { foo: bar }, false=> {foo:bar}
+  jsxBracketSameLine: false, // jsx闭标签是否另起一行 <Test></Test>
+  arrowParens: 'avoid', // 箭头函数是否省略括号 a=> a, (a)=> a
+  singleQuote: true, // 单引号
+  semi: true, //是否在语句末尾就分号
+};
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,17 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <Login> </Login>
   </q-page>
 </template>
 
 <script>
+import Login from './Login';
+
 export default {
-  name: 'PageIndex'
-}
+  name: 'PageIndex',
+  comments: {
+    Login,
+  },
+};
 </script>
