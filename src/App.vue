1630808a1620ae7a1070e0c9e29fcbca813bbeed
Luke Lin <181977814@qq.com>
2020-02-01 22:33:15 +0800

    Add modules example.

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,7 @@ export default {
   name: 'App'
 }
 </script>
+<style lang="scss">
+@import '../node_modules/ag-grid-community/dist/styles/ag-grid.css';
+@import '../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css';
+</style>
--- src/pages/modules/modules.vue
+++ src/pages/modules/modules.vue
@@ -1,29 +1,95 @@
 <template>
   <q-page class="flex flex-center">
-    this is modules.
-    <q-btn color="primary" @click="$auth.logout()">
-      no logout
-    </q-btn>
-    <q-btn color="primary" @click="getZModules()">
-      测试state
-    </q-btn>
+    <q-btn v-on:click="test()">asdfasdfs</q-btn>
+    <q-card>
+      <ag-grid-vue
+        style="width: 1000px; height: 500px;"
+        class="ag-theme-balham"
+        @grid-ready="onGridReady"
+        :columnDefs="columnDefs"
+        :rowData="rowData"
+        :defaultColDef="defaultColDef"
+      >
+      </ag-grid-vue>
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import { AgGridVue } from 'ag-grid-vue'
 
 export default {
   name: 'modules',
-  computed: {
-    ...mapState('zero', ['products'])
-  },
-  methods: {
-    ...mapActions('zero', ['getZModules']),
-    test() {
-      console.log('Its a test')
+  computed: {},
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null,
+      defaultColDef: null
     }
   },
-  components: {}
+  beforeMount() {
+    this.columnDefs = [
+      {
+        headerName: '模块名',
+        field: 'name',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: '标题',
+        field: 'title',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: 'ICON',
+        field: 'icon',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: '是否菜单',
+        field: 'ismenu',
+        sortable: true,
+        filter: true
+      },
+      {
+        headerName: '路径名',
+        field: 'url',
+        sortable: true,
+        filter: true
+      }
+    ]
+    this.defaultColDef = {
+      editable: true,
+      resizable: true
+    }
+  },
+  components: {
+    AgGridVue
+  },
+  mounted() {
+    // console.log(this.ZModules)
+    this.$router.app.$http
+      .get('/z_module/')
+      .then(res => {
+        if (res.data.success) {
+          // console.log(res.data.data)
+          this.rowData = res.data.data
+        } else {
+        }
+      })
+      .catch(e => {})
+  },
+  methods: {
+    // ...mapActions('zero', ['getZModules']),
+    onGridReady(params) {
+      params.api.sizeColumnsToFit()
+    },
+    test() {
+      console.log(this.rowData)
+    }
+  }
 }
 </script>
