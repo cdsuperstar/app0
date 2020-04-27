718bd0a7249edf74f52a58d157e94b7eba1c1c39
181977814@qq.com <123ilovegit>
2020-04-27 08:59:00 +0800

    fix message.vue 0427...

--- src/pages/modules/message.vue
+++ src/pages/modules/message.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="q-pa-md" align="center">
     <span style="color: #9C9C9C">温馨提示：已阅消息将于30天后自动清理</span>
-    <div class="q-pa-ma row items-start q-gutter-md">
-      <q-card flat width="500px">
+    <div class="row items-start q-gutter-md">
+      <q-card flat style="margin-left:6px;">
         <q-card-section style="padding: 1px;">
           <q-tabs
             v-model="tab"
@@ -53,174 +53,164 @@
           </q-tabs>
         </q-card-section>
       </q-card>
-      <q-card flat style="max-width: 1000px;">
-        <q-card-section>
-          <q-tab-panels
-            v-model="tab"
-            animated
-            class="text-left"
-            transition-prev="jump-up"
-            transition-next="jump-up"
-          >
-            <q-tab-panel name="AllMsg">
-              <q-timeline
-                :layout="!$q.screen.gt.xs ? 'dense' : 'comfortable'"
-                side="right"
-                color="secondary"
+      <q-card flat style="max-width: 800px;">
+        <q-tab-panels
+          v-model="tab"
+          animated
+          class="text-left"
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="AllMsg">
+            <q-timeline
+              :layout="!$q.screen.gt.xs ? 'dense' : 'comfortable'"
+              side="right"
+              color="secondary"
+            >
+              <!--          <q-timeline-entry heading class="text-left"-->
+              <!--            ><font size="5">消息列表</font></q-timeline-entry-->
+              <!--          >-->
+              <q-timeline-entry
+                class="text-left"
+                title="XXX模块消息"
+                subtitle="2020年4月22日 16：37"
+                side="left"
+                avatar="https://cdn.quasar.dev/img/avatar3.jpg"
               >
-                <!--          <q-timeline-entry heading class="text-left"-->
-                <!--            ><font size="5">消息列表</font></q-timeline-entry-->
-                <!--          >-->
-                <q-timeline-entry
-                  class="text-left"
-                  title="XXX模块消息"
-                  subtitle="2020年4月22日 16：37"
-                  side="left"
-                  avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-                >
-                  <q-item clickable v-ripple>
-                    <q-item-section>
-                      关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                    </q-item-section>
-                  </q-item>
-                </q-timeline-entry>
-                <q-timeline-entry
-                  class="text-left"
-                  title="XXX模块消息"
-                  subtitle="2020年4月22日 16：37"
-                  side="left"
-                  avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-                >
-                  <q-item clickable v-ripple>
-                    <q-item-section>
-                      关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                    </q-item-section>
-                  </q-item>
-                </q-timeline-entry>
-                <q-timeline-entry
-                  class="text-left"
-                  title="XXX模块消息"
-                  subtitle="2020年4月22日 16：37"
-                  side="left"
-                  avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-                >
-                  <q-item clickable v-ripple>
-                    <q-item-section>
-                      关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                    </q-item-section>
-                  </q-item>
-                </q-timeline-entry>
-                <q-timeline-entry
-                  class="text-left"
-                  title="XXX模块消息"
-                  subtitle="2020年4月22日 16：37"
-                  side="left"
-                  avatar="https://cdn.quasar.dev/img/avatar3.jpg"
-                >
-                  <q-item clickable v-ripple>
-                    <q-item-section>
-                      关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                      <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Quis praesentium cumque magnam odio iure quidem,
-                        quod illum numquam possimus obcaecati commodi minima
-                        assumenda consectetur culpa fuga nulla ullam. In,
-                        libero.
-                      </p>
-                    </q-item-section>
-                  </q-item>
-                </q-timeline-entry>
-              </q-timeline>
-            </q-tab-panel>
+                <q-item clickable v-ripple>
+                  <q-item-section>
+                    关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                  </q-item-section>
+                </q-item>
+              </q-timeline-entry>
+              <q-timeline-entry
+                class="text-left"
+                title="XXX模块消息"
+                subtitle="2020年4月22日 16：37"
+                side="left"
+                avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              >
+                <q-item clickable v-ripple>
+                  <q-item-section>
+                    关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                  </q-item-section>
+                </q-item>
+              </q-timeline-entry>
+              <q-timeline-entry
+                class="text-left"
+                title="XXX模块消息"
+                subtitle="2020年4月22日 16：37"
+                side="left"
+                avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              >
+                <q-item clickable v-ripple>
+                  <q-item-section>
+                    关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                  </q-item-section>
+                </q-item>
+              </q-timeline-entry>
+              <q-timeline-entry
+                class="text-left"
+                title="XXX模块消息"
+                subtitle="2020年4月22日 16：37"
+                side="left"
+                avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+              >
+                <q-item clickable v-ripple>
+                  <q-item-section>
+                    关于聘任2020年度教学督导督查组成员的通知-各单位（部门）内部收文处理笺
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Quis praesentium cumque magnam odio iure quidem, quod
+                      illum numquam possimus obcaecati commodi minima assumenda
+                      consectetur culpa fuga nulla ullam. In, libero.
+                    </p>
+                  </q-item-section>
+                </q-item>
+              </q-timeline-entry>
+            </q-timeline>
+          </q-tab-panel>
 
-            <q-tab-panel name="Sysmsg">
-              <div class="text-h4 q-mb-md">Alarms</div>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-                praesentium cumque magnam odio iure quidem, quod illum numquam
-                possimus obcaecati commodi minima assumenda consectetur culpa
-                fuga nulla ullam. In, libero.
-              </p>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-                praesentium cumque magnam odio iure quidem, quod illum numquam
-                possimus obcaecati commodi minima assumenda consectetur culpa
-                fuga nulla ullam. In, libero.
-              </p>
-            </q-tab-panel>
+          <q-tab-panel name="Sysmsg">
+            <div class="text-h4 q-mb-md">Alarms</div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+          </q-tab-panel>
 
-            <q-tab-panel name="UserMsg">
-              <div class="text-h4 q-mb-md">Movies</div>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-                praesentium cumque magnam odio iure quidem, quod illum numquam
-                possimus obcaecati commodi minima assumenda consectetur culpa
-                fuga nulla ullam. In, libero.
-              </p>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-                praesentium cumque magnam odio iure quidem, quod illum numquam
-                possimus obcaecati commodi minima assumenda consectetur culpa
-                fuga nulla ullam. In, libero.
-              </p>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
-                praesentium cumque magnam odio iure quidem, quod illum numquam
-                possimus obcaecati commodi minima assumenda consectetur culpa
-                fuga nulla ullam. In, libero.
-              </p>
-            </q-tab-panel>
-          </q-tab-panels>
-        </q-card-section>
+          <q-tab-panel name="UserMsg">
+            <div class="text-h4 q-mb-md">Movies</div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+          </q-tab-panel>
+        </q-tab-panels>
       </q-card>
     </div>
   </div>
@@ -255,4 +245,8 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.q-tab {
+  padding: 0 14px;
+}
+</style>
