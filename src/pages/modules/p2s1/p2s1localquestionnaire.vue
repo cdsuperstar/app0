509c341d8d1f3dd76ce8927124520dd8d2eb5388
Homer <181977814@qq.com>
2020-05-23 18:44:07 +0800

    fix some bugs0523...

---
+++ src/pages/modules/p2s1/p2s1localquestionnaire.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="q-ma-ma">本地问卷</q-page>
+</template>
+
+<script>
+export default {
+  name: 'P2s1localquestionnaire'
+}
+</script>
+<style scoped></style>
---
+++ src/pages/modules/p2s1/p2s1setquestionnaire.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-ma-ma">管理问卷</q-page>
+</template>
+
+<script>
+export default {
+  name: 'P2s1setquestionnaire'
+}
+</script>
+
+<style scoped></style>
