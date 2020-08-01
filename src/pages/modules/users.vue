a081d7e0b899a850f2a3733ab1b1c433b0b0ca34
Luke Lin <181977814@qq.com>
2020-08-01 15:42:20 +0800

    Checkout from app3.

--- src/pages/modules/users.vue
+++ src/pages/modules/users.vue
@@ -393,6 +393,8 @@ export default {
     },
     initGrid() {
       this.gridOptions = {
+        rowHeight: 32,
+        headerHeight: 32,
         allowShowChangeAfterFilter: true
       }
       this.columnDefs = [
@@ -757,9 +759,15 @@ function pwdMask(params) {
 .User-agGrid .ag-header {
   background-color: var(--q-color-secondary);
   color: #ffffff;
+  font-size: 13px;
 }
 .User-agGrid .ag-cell {
   padding-left: 1px;
+  font-size: 13px;
+}
+.ag-theme-balham .ag-ltr .ag-cell {
+  padding-left: 1px;
+  border-right: 1px solid rgba(233, 233, 233, 0.96);
 }
 .ag-theme-balham .ag-icon,
 .ag-header-icon .ag-sort-ascending-icon {
